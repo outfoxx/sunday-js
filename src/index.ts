c0ce34f937c946873c80fe40c16b4c1cb14d7c35
Kevin Wooten <kevin@outfoxx.io>
2020-09-14 17:56:44 -0700

    fix: export public rxjs utils

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ export * from './any-type';
 export * from './request-manager';
 export * from './fetch-request-manager';
 
+export { nullifyNotFound } from './util/rxjs';
+
 import { decode, encode, encodeSlice } from './util/base64';
 export const Base64 = {
   decode,
