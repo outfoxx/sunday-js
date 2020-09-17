d0e8e975b651dd37cd1e5601a8fcf94898f33864
Kevin Wooten <kevin@outfoxx.io>
2020-09-17 14:49:48 -0700

    fix: export all useful types

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
-export * from './problem';
 export * from './any-type';
 export * from './class-type';
 export * from './request-factory';
 export * from './fetch-request-factory';
 export * from './url-template';
+export * from './media-type-decoder';
+export * from './media-type-decoders';
+export * from './media-type-encoder';
+export * from './media-type-encoders';
+export * from './json-decoder';
+export * from './json-encoder';
+export * from './binary-decoder';
+export * from './binary-encoder';
+export * from './url-encoder';
+export * from './logger';
+export * from './http-error';
+export * from './problem';
 
 export { nullifyNotFound } from './util/rxjs';
 
