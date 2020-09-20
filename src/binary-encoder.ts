2fa7c92abe3012fe11afdf0ae2bd290d3e4740c0
Kevin Wooten <kevin@outfoxx.io>
2020-09-20 09:44:25 -0700

    fix(binary/encoder): allow blobs & streams to be binary encoded

--- src/binary-encoder.ts
+++ src/binary-encoder.ts
@@ -3,9 +3,16 @@ import { MediaTypeEncoder } from './media-type-encoder';
 export class BinaryEncoder implements MediaTypeEncoder {
   static default = new BinaryEncoder();
 
-  encode(value: unknown): BufferSource {
-    if (!ArrayBuffer.isView(value) && !(value instanceof ArrayBuffer)) {
-      throw Error('Invalid value, expected BufferSource');
+  encode(value: unknown): BodyInit {
+    if (
+      !ArrayBuffer.isView(value) &&
+      !(value instanceof ArrayBuffer) &&
+      !(value instanceof Blob) &&
+      !(value instanceof ReadableStream)
+    ) {
+      throw Error(
+        'Invalid value, expected BufferSource, Blob or ReadableStream'
+      );
     }
     return value;
   }
