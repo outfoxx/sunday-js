aec6636c9bdaf0e603c050be7c1ec2c8c37d6761
Kevin Wooten <kevin@outfoxx.io>
2020-09-20 10:49:38 -0700

    fix: export date/time types correctly

--- src/date-time-types.ts
+++ src/date-time-types.ts
@@ -1,5 +1,10 @@
 import { DateTime as luxonDateTime } from 'luxon';
 
+export type Date = luxonDateTime;
 export const Date = luxonDateTime;
+
+export type Time = luxonDateTime;
 export const Time = luxonDateTime;
+
+export type DateTime = luxonDateTime;
 export const DateTime = luxonDateTime;
